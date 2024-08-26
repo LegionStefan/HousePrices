37d74f8fa4cbfe7e77a4f83388384097f01d3702
Snemoko <90250493+Snemoko@users.noreply.github.com>
2024-08-26 22:22:10 +0200

    Wrote it so that the retrieveHousePrices is within a anonymous function and we only have to setloading false once

--- frontend/src/app/components/HousePricesComponent.tsx
+++ frontend/src/app/components/HousePricesComponent.tsx
@@ -1,30 +1,31 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 export default function HousePricesComponent() {
-    const [data, setData] = useState(null); // State to store the fetched data
+    const [housePrices, setHousePrices] = useState(null); // State to store the fetched data
     const [loading, setLoading] = useState(true); // State to manage loading state
     const [error, setError] = useState(null); // State to manage errors
 
     useEffect(() => {
         // Fetch data from the API when the component mounts
-        fetch('http://localhost:8080/housePrices')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                setData(data); // Update state with the fetched data
-                setLoading(false); // Set loading to false as data is fetched
-            })
-            .catch((error) => {
-                setError(error.message); // Handle any errors
-                setLoading(false); // Set loading to false as there's an error
-            });
-    }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+        const retrieveHousePrices = async () => {
+            fetch('http://localhost:8080/housePrices')
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    setHousePrices(data);
+                })
+                .catch((error) => {
+                    setError(error.message);
+                });
+        };
+        retrieveHousePrices().finally(() => setLoading(false));
+    }, []);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -37,7 +38,7 @@ export default function HousePricesComponent() {
     return (
         <div>
             <h1>Fetched Data:</h1>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <pre>{JSON.stringify(housePrices, null, 2)}</pre>
         </div>
     );
 }
