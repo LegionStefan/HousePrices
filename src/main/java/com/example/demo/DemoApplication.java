32bce6f195fc890df1bd11dc0413b47d09539ba8
Snemoko <90250493+Snemoko@users.noreply.github.com>
2024-08-27 20:12:38 +0200

    Eerste start gemaakt voor een scheduled task

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.config.WebConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
